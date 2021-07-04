49b94bc2be75e84c73cf35275cf38575f2e5bba1
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-04 15:00:59 +0200

    Add files via upload

---
+++ Javascript basics/index.js
@@ -0,0 +1,30 @@
+console.log("Hello Winc Academy")
+
+let name = 'Linda';
+console.log ("name")
+
+let number = '4+4'
+console.log ("number")
+
+String number = "4";
+String number = "4";
+String str = "4" + "4";
+console.log ("number")
+
+let number = "5-2"
+console.log ("number")
+
+let number = "5*2"
+console.log ("number")
+
+let number = "6/2"
+console.log ("number")
+
+let number = "10%50"
+console.log ("number")
+
+let age = "37";
+console.log ("age")
+
+let age = "zevenendertig";
+console.log ("age")
