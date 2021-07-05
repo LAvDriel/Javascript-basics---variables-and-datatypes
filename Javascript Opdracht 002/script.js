a4bf689b2f23e08c7c76f25f3efda0cf5f6fbf64
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-05 17:46:13 +0200

    Add files via upload

---
+++ Javascript Opdracht 002/script.js
@@ -0,0 +1,26 @@
+let person = {
+    firstName: Linda 
+    age: 37
+    evaluations: 7
+    evaluations: 10
+    evaluations: 9
+    hobby: fitness
+}
+person.firstName = "Linda";
+console.log ("person.firstName");
+person.age = "37"
+console.log ("person.age");
+
+person [firstName] = "Linda"
+person [age] = "37"
+
+person.evaluations = "7";
+console.log ("evaluations")
+
+let haarkleur ["blond", "bruin", "rood"]
+console.log ("haarkleur")
+
+console.log(haarkleur.length);
+
+person.firstName = "Linda";
+console.log ("person.firstName");
