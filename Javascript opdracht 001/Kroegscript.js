91938b45e67a379a5b6d0d201d48addbe3d935ae
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-05 15:48:12 +0200

    Add files via upload

---
+++ Javascript opdracht 001/Kroegscript.js
@@ -0,0 +1,26 @@
+const age = 37
+if (age > 17) {
+    document.write (mag die persoon naar binnen)
+    }
+
+else {
+    document.write (blijf alsjeblieft buiten wachten)
+}
+console.log ("age:", age);
+
+
+const isFemale = true
+if (isFemale) {
+    document.write (mag die persoon naar binnen)
+} else {
+    document.write (mag die persoon niet naar binnen)
+}
+console.log ("isFemale:", isFemale);
+console.log ("false:", false);
+
+const driverStatus = bob
+if (bob) {
+    document.write (mag die persoon rijden)
+}
+console.log ("driverStatus:", driverStatus)
+console.log ("notBob:", notBob)
--- Javascript opdracht 001/script.js
+++ Javascript opdracht 001/script.js
@@ -1,5 +1,16 @@
 console.log("Hallo Winc Academy studenten")
 
+/ Dit is een grote som
+
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/* Ik ben een blok comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+
 /* Blok comment, schakelt code uit
 // Dit is een grote som
 let multiplication = "1230941 * 1823794";
